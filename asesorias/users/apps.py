90abd76e196306a302b6b30007e91767baadd52b
Armando-Alamilla <armando_alamilla10@hotmail.com>
2020-03-29 21:22:45 -0600

    utils module and users app created

---
+++ asesorias/users/apps.py
@@ -0,0 +1,11 @@
+"""Users app."""
+
+# Django imports
+from django.apps import AppConfig
+
+
+class UsersAppConfig():
+    """Users app config."""
+
+    name = 'asesorias.users'
+    verbose_name = 'Users'
